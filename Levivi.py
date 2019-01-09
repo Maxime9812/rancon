7ee6b4c0610041b3e97dc388b8a954ccfe655b58
Maxime Le Duc <maximeld19@gmail.com>
2019-01-10 00:52:34 +0100

    decodeur

--- Levivi.py
+++ Levivi.py
@@ -1,3 +1,7 @@
+TGREEN = '\033[32m'  # Green Text
+TRED = '\033[31m'  # Red Text
+ENDC = '\033[m'  # reset to the defaults
+
 f = open("C:/Users/Maxime/Desktop/mdr/test.txt", "r")
 test = str(f.read())
 list = [bin(ord(element)) for element in test]
@@ -5,3 +9,6 @@ test2 = ''.join(list)
 f = open("C:/Users/Maxime/Desktop/mdr/test.txt", "w+")
 f.write(test2)
 f.close()
+b = open("C:/Users/Maxime/Desktop/mdr/decodeur.py", "w+")
+b.write("import os  \r\nos.mkdir('C:/Users/Maxime/Desktop/mdr/1')")
+print(TGREEN +'hacked', ENDC)
